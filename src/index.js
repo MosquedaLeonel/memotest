ca9b671c9a098eed9490c5a300e4a5bca49fde98
MosquedaLeonel <78828652+MosquedaLeonel@users.noreply.github.com>
2021-12-11 15:09:13 -0300

    agrega la configuracion inicial del juego

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const btnStart = document.querySelector('#btn-start');
+const btnRestart = document.querySelector('#btn-restart');
+
+const imagesBase = ['boston-celtics', 'chicago-bulls', 'golden-state-warriors', 'houston-rockets', 'los-angeles-lakers', 'miami-heat', 'phoenix-suns', 'san-antonio-spurs'];
+
+const $boardGame = document.querySelector('#board');
+const $cards = $boardGame.querySelectorAll('.cards');
+
+let score = 0;
+let count = 0;
+let firstCard = null;
+
